798038cbdadcff67e66653e3cf817384d6e92995
Claire <cameron.liam.yon@gmail.com>
2026-02-05 11:30:26 -0500

    added constructor to employee

--- lab3/Employee.java
+++ lab3/Employee.java
@@ -4,8 +4,11 @@ public abstract class Employee {
     private String lastName;
     private String socialSecurityNumber;
  
-    // TODO 1: Create a constructor
-    
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
     // TODO 2: Create getters
  
     // TODO 3: Declare abstract method earnings()
