e97bf5f202face52799fb10080c4564e408f7a1f
Claire <cameron.liam.yon@gmail.com>
2026-02-05 11:45:58 -0500

    added getters and setters to Commission Employee, replaced "TODOs" up until this point with comments

--- lab3/CommissionEmployee.java
+++ lab3/CommissionEmployee.java
@@ -1,17 +1,28 @@
 public class CommissionEmployee extends Employee {
- 
+    // Variable delcaration
     private double grossSales;
     private double commissionRate;
  
-
+    // Constructor for Object
     public CommissionEmployee(String firstName, String lastName, String ssn, double sales, double rate) {
         super(firstName, lastName, ssn);
         setGrossSales(sales);
         setCommissionRate(rate);
     }
  
-    // TODO 6: Create getters and setters with validation
- 
+    // Methods to set variables
+    public void setGrossSales(double sales) {
+        if (sales < 0.0) throw new IllegalArgumentException("Gross sales must be above or equal to 0.0");
+        grossSales = sales;
+    }
+    public void setCommissionRate(double rate) {
+        if (rate <= 0.0 || rate >= 1.0) throw new IllegalArgumentException("Commission rate must be above 0.0 and below 1.0");
+        commissionRate = rate;
+    }
+
+    // Methods to get variables
+    public double getGrossSales() { return grossSales; }
+    public double getCommissionRate() { return commissionRate; }
     // TODO 7: Override earnings()
  
     // TODO 8: Override toString()
--- lab3/Employee.java
+++ lab3/Employee.java
@@ -1,24 +1,25 @@
 public abstract class Employee {
- 
+    // Declare Variables
     private String firstName;
     private String lastName;
     private String socialSecurityNumber;
- 
+
+    // Construct Object
     public Employee(String firstName, String lastName, String socialSecurityNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.socialSecurityNumber = socialSecurityNumber;
     }
 
-    
+    // Methods to get each variable
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
     public String getSSN() { return socialSecurityNumber; }
 
-
+    // Delcare abstract
     public abstract double earnings();
  
-
+    // Override the to string method and create a formatted string
     @Override
     public String toString() {
         return String.format("%s %s%nSocial Security Number: %s",
